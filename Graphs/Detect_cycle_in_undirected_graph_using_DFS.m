030c1fc1b609c235ba20c0508c042e127ce2f8f9
Vishwas Joshi <98074283+vishwasjoshi2019@users.noreply.github.com>
2023-08-09 11:19:25 +0530

    Update Detect_cycle_in_undirected_graph_using_DFS.m

--- Graphs/Detect_cycle_in_undirected_graph_using_DFS.m
+++ Graphs/Detect_cycle_in_undirected_graph_using_DFS.m
@@ -1,1 +1,54 @@
 
+# Detect cycle in an undirected graph using DFS
+
+## [Question](https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1)
+
+Given an undirected graph with V vertices and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with
+
+---
+
+## Solution
+
+```cpp
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    
+    bool cycle_check(int node, int parent,vector<int> adj[], vector<int>&visited){
+        
+        visited[node]=1;
+        
+        for(auto it:adj[node]){ // for(int i=0;i<adj[node].size();i++)
+            
+            if(visited[it]==0){
+                if(cycle_check(it,node,adj,visited)){
+                    return true;
+                }
+            }
+            else if(it!=parent){
+                return true;
+            }
+        }
+        
+        
+        return false;
+        
+    }
+    
+    
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> visited(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(cycle_check(i,-1,adj,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+        // Code here
+        
+    }
+};
